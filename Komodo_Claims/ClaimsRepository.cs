14ba56bda1ff8f659b26728821b46d7cbb01b524
RiQ <88129539+RiQ472@users.noreply.github.com>
2021-08-29 20:32:12 -0400

    Update ClaimsRepository.cs

--- Komodo_Claims/ClaimsRepository.cs
+++ Komodo_Claims/ClaimsRepository.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace Komodo_Claims
 {
-    class ClaimsRepository
+    public class ClaimsRepository
     {
+        public readonly Queue<Claims> _claims = new Queue<Claims>();
+
+        public Queue<Claims> AllClaims()
+        {
+            return _claims;
+        }
+        public bool NextClaim(Claims nextClaim)
+        {
+            Claims contentToQueue = nextClaim;
+            if (contentToQueue != null)
+            {
+                _claims.Dequeue();
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public Claims PeekNextClaim()
+        {
+            return _claims.Peek();
+        }
+
+        public bool Addclaim(Claims newClaim)
+        {
+            int startingCount = _claims.Count;
+            _claims.Enqueue(newClaim);
+            bool claimAdded = (_claims.Count > startingCount) ? true : false;
+            return claimAdded;
+        }
     }
 }
